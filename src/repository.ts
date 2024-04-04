d0c1be70bd73b817fa9efae356ebe122e7430941
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-04 10:06:08 +0200

    feat: initial commit
    Extracted from hello.nrfcloud.com/backend

---
+++ src/repository.ts
@@ -0,0 +1,49 @@
+import {
+	CreateRepositoryCommand,
+	DescribeRepositoriesCommand,
+	ECRClient,
+	ImageTagMutability,
+	RepositoryNotFoundException,
+} from '@aws-sdk/client-ecr'
+
+type logFn = (...args: string[]) => void
+
+export type ContainerRepository = {
+	uri: string
+	name: string
+}
+
+export const getOrCreateRepository =
+	({ ecr }: { ecr: ECRClient }) =>
+	async (name: string, debug?: logFn): Promise<ContainerRepository> => {
+		try {
+			const result = await ecr.send(
+				new DescribeRepositoriesCommand({
+					repositoryNames: [name],
+				}),
+			)
+			const uri = result.repositories?.[0]?.repositoryUri ?? ''
+			return {
+				name,
+				uri,
+			}
+		} catch (error) {
+			if (error instanceof RepositoryNotFoundException) {
+				debug?.(`Repository ${name} does not exist. Create a new repository.`)
+				// Create a repository
+				const result = await ecr.send(
+					new CreateRepositoryCommand({
+						repositoryName: name,
+						imageTagMutability: ImageTagMutability.IMMUTABLE,
+					}),
+				)
+
+				return {
+					name,
+					uri: result.repository?.repositoryUri ?? '',
+				}
+			} else {
+				throw error
+			}
+		}
+	}
